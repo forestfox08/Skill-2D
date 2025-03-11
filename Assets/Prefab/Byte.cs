4346d1abb292323504a538057e4fbb19fccaf249
forestfox08 <j.rdb08@hotmail.com>
2025-03-11 12:42:52 +0100

    (*^▽^*)

--- Assets/Prefab/Byte.cs
+++ Assets/Prefab/Byte.cs
@@ -20,12 +20,14 @@ public class Byte : MonoBehaviour
 
     private void Bin2Dec()
     {
-        Debug.Log("wiwiwi");
-
-        this.value = 0;
-        //if (bits[0].state)
-        {
-           //this.
-        }
+        value = 0;
+        if (bits[0].state) { value += 1; }
+        if (bits[1].state) { value += 2; }
+        if (bits[2].state) { value += 4; }
+        if (bits[3].state) { value += 8; }
+        if (bits[4].state) { value += 16;}
+        if (bits[5].state) { value += 32;}
+        if (bits[6].state) { value += 64;}
+        if (bits[7].state) { value += 128;}
     }
 }
